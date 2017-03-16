bb7b01fcc25af1b606e00ea244dd2309c025a726
hayashi <yk_15@outlook.jp>
2017-03-16 19:25:33 +0900

    htdoocs -> docs に変更。 しかもそもそも htdoocs の o が一個多かったです。 ごめんなさい。今気づきました。すみません。

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var port = 7270;
 var express = require('express');
 var app = express();
 
-app.use(express.static('htdoocs'));
+app.use(express.static('docs'));
 
 app.listen(port, function() {
   console.log("Express: Port: %d, Mode: %s", port, app.settings.env)
